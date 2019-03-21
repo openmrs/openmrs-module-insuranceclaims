410370bc95c0ff5ef884ff25ff766a0429aaf8b7
Arkadiusz Lalo <alalo@soldevelo.com>
2019-03-21 17:56:09 +0100

    ICLM-3: Generated the initial project of a module

---
+++ api/src/main/java/org/openmrs/module/insuranceclaims/api/impl/InsuranceClaimsServiceImpl.java
@@ -0,0 +1,51 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.insuranceclaims.api.impl;
+
+import org.openmrs.api.APIException;
+import org.openmrs.api.UserService;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.insuranceclaims.Item;
+import org.openmrs.module.insuranceclaims.api.InsuranceClaimsService;
+import org.openmrs.module.insuranceclaims.api.dao.InsuranceClaimsDao;
+
+public class InsuranceClaimsServiceImpl extends BaseOpenmrsService implements InsuranceClaimsService {
+	
+	private InsuranceClaimsDao dao;
+	
+	private UserService userService;
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setDao(InsuranceClaimsDao dao) {
+		this.dao = dao;
+	}
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Override
+	public Item getItemByUuid(String uuid) throws APIException {
+		return dao.getItemByUuid(uuid);
+	}
+	
+	@Override
+	public Item saveItem(Item item) throws APIException {
+		if (item.getOwner() == null) {
+			item.setOwner(userService.getUser(1));
+		}
+		
+		return dao.saveItem(item);
+	}
+}
---
+++ api/src/test/java/org/openmrs/module/insuranceclaims/InsuranceClaimsServiceTest.java
@@ -0,0 +1,64 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.insuranceclaims.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.openmrs.User;
+import org.openmrs.api.UserService;
+import org.openmrs.module.insuranceclaims.Item;
+import org.openmrs.module.insuranceclaims.api.dao.InsuranceClaimsDao;
+import org.openmrs.module.insuranceclaims.api.impl.InsuranceClaimsServiceImpl;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * This is a unit test, which verifies logic in InsuranceClaimsService. It doesn't extend
+ * BaseModuleContextSensitiveTest, thus it is run without the in-memory DB and Spring context.
+ */
+public class InsuranceClaimsServiceTest {
+	
+	@InjectMocks
+	InsuranceClaimsServiceImpl basicModuleService;
+	
+	@Mock
+	InsuranceClaimsDao dao;
+	
+	@Mock
+	UserService userService;
+	
+	@Before
+	public void setupMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void saveItem_shouldSetOwnerIfNotSet() {
+		//Given
+		Item item = new Item();
+		item.setDescription("some description");
+		
+		when(dao.saveItem(item)).thenReturn(item);
+		
+		User user = new User();
+		when(userService.getUser(1)).thenReturn(user);
+		
+		//When
+		basicModuleService.saveItem(item);
+		
+		//Then
+		assertThat(item, hasProperty("owner", is(user)));
+	}
+}
